87350ab9dd00f7554ea944649fdc200517f17916
Alexander Kureniov <s@zxc.pp.ua>
2018-07-28 23:13:02 +0300

    ci: try to fix tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   collectCoverage: true,
   verbose: true,
-  testPathIgnorePatterns: ['/node_modules/', 'static', 'build'],
+  testPathIgnorePatterns: ['node_modules', 'static', 'build'],
 };
