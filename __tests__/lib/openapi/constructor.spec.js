096b3fb9ea60d03fd22dff06a32697a0f665801e
Alexander Kureniov <s@zxc.pp.ua>
2018-07-31 00:27:02 +0300

    test(openapi): add initial openapi validation test

--- __tests__/lib/openapi/constructor.spec.js
+++ __tests__/lib/openapi/constructor.spec.js
@@ -5,25 +5,74 @@ const swParser = require('swagger-parser');
 
 const openapi = require('../../../lib/openapi/constructor');
 
-
 describe('openapi constructor', () => {
   describe('swagger 2.0 compatible', () => {
     const options = {
       swagger: {
-
+        info: {
+          title: 'Test swagger',
+          description: 'testing the fastify swagger api',
+          version: '0.1.0',
+        },
+        host: 'localhost',
+        schemes: ['http'],
+        consumes: ['application/json'],
+        produces: ['application/json'],
+        tags: [{name: 'tag'}],
+        externalDocs: {
+          description: 'Find more info here',
+          url: 'https://swagger.io',
+        },
+        securityDefinitions: {
+          apiKey: {
+            type: 'apiKey',
+            name: 'apiKey',
+            in: 'header',
+          },
+        },
+        security: [
+          {
+            apiKey: [],
+          },
+        ],
       },
     };
-    test('test', () => {
+    test('generates valid swagger header', async () => {
+      const api = openapi({options})();
+      expect(api).toEqual({
+        openapi: '3.0.0',
+        info: {
+          title: 'Test swagger',
+          description: 'testing the fastify swagger api',
+          version: '0.1.0',
+        },
+        components: {
+          securitySchemes: {
+            apiKey: {
+              type: 'apiKey',
+              name: 'apiKey',
+              in: 'header',
+            },
+          },
+        },
+        tags: [{name: 'tag'}],
+        externalDocs: {
+          description: 'Find more info here',
+          url: 'https://swagger.io',
+        },
+        security: [{apiKey: []}],
+        servers: [{url: 'http://localhost/'}],
+        paths: {},
+      });
+      expect(swParser.validate(api)).resolves.toEqual(api);
     });
   });
 
   describe('openapi 3.0 compatible', () => {
-    test('test', () => {
-    });
+    test('test', () => {});
   });
 
   describe('openapi 3.0 defaults', () => {
-    test('test', () => {
-    });
+    test('test', () => {});
   });
 });
