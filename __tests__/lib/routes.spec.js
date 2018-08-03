734756b41a74133bfb939abfebe441498c963753
Alexander Kureniov <s@zxc.pp.ua>
2018-08-03 12:46:17 +0300

    test: fix routes test

--- __tests__/lib/routes.spec.js
+++ __tests__/lib/routes.spec.js
@@ -9,18 +9,20 @@ describe('plugin', async () => {
   app.register(oasPlugin, {exposeRoute: true});
   // await app.ready();
   test('redirects to documentation index', (done) => {
-    app.inject(
-      {
-        method: 'GET',
-        url: '/documentation',
-      },
-      (err, res) => {
-        expect(res.statusCode).toEqual(302);
-        expect(res.headers['location']).toEqual('/documentation/index.html');
-        expect(typeof res.payload).toBe('string');
-        done();
-      }
-    );
+    app.ready().then(() => {
+      app.inject(
+        {
+          method: 'GET',
+          url: '/documentation',
+        },
+        (err, res) => {
+          expect(res.statusCode).toEqual(302);
+          expect(res.headers['location']).toEqual('/documentation/index.html');
+          expect(typeof res.payload).toBe('string');
+          done();
+        }
+      );
+    });
   });
   test('returns documentation', (done) => {
     app.ready().then(() => {
@@ -40,32 +42,36 @@ describe('plugin', async () => {
   });
 
   test('returns json spec', (done) => {
-    app.inject(
-      {
-        method: 'GET',
-        url: '/documentation/json',
-      },
-      (err, res) => {
-        expect(err).toBeFalsy();
-        expect(res.statusCode).toEqual(200);
-        expect(res.payload).toBeDefined();
-        done();
-      }
-    );
+    app.ready().then(() => {
+      app.inject(
+        {
+          method: 'GET',
+          url: '/documentation/json',
+        },
+        (err, res) => {
+          expect(err).toBeFalsy();
+          expect(res.statusCode).toEqual(200);
+          expect(res.payload).toBeDefined();
+          done();
+        }
+      );
+    });
   });
 
   test('returns yaml spec', (done) => {
-    app.inject(
-      {
-        method: 'GET',
-        url: '/documentation/yaml',
-      },
-      (err, res) => {
-        expect(err).toBeFalsy();
-        expect(res.statusCode).toEqual(200);
-        expect(res.payload).toBeDefined();
-        done();
-      }
-    );
+    app.ready().then(() => {
+      app.inject(
+        {
+          method: 'GET',
+          url: '/documentation/yaml',
+        },
+        (err, res) => {
+          expect(err).toBeFalsy();
+          expect(res.statusCode).toEqual(200);
+          expect(res.payload).toBeDefined();
+          done();
+        }
+      );
+    });
   });
 });
